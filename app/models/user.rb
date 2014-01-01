f988ff5462b5eeaba7564e4ff333907b2f3e3dbe
Agolveo <agolveo@gmail.com>
2014-01-01 15:40:39 -0800

    added friendship functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,10 +1,14 @@
 class User < ActiveRecord::Base
 	has_secure_password
-	validates_presence_of :username, :email
-	validates 			  :username, :email, uniqueness: true
-	validates             :username, length: { minimum: 3, maximum: 10 }
-	validates_format      :email, :with => /\w+@\w+\.\w+/i
+	validates_presence_of :name, :email
+	validates 			  :name, :email, uniqueness: true
+	validates             :name, length: { minimum: 3, maximum: 10 }
+	validates_format_of   :email, :with => /\w+@\w+\.\w+/i
 
 	has_many :friendships
 	has_many :friends, through: :friendships
+	has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
+	has_many :inverse_friends, through: :inverse_friendships, source: :user
+
+
 end
